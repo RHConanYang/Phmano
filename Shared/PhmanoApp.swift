3a198697d54593e3a596ba0a92cc5b6c00932cfd
Conan <millduos@gmail.com>
2021-01-07 22:02:37 +0800

    Initial Commit

---
+++ Shared/PhmanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhmanoApp.swift
+//  Shared
+//
+//  Created by 楊仁翰 on 2021/1/7.
+//
+
+import SwiftUI
+
+@main
+struct PhmanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
